5cde10f201ce96c9591fe3574f45c50f07f1f159
Aj <52761599+Captain-X-X@users.noreply.github.com>
2025-06-02 04:25:32 +0000

    Create build.sh

---
+++ suckless/build.sh
@@ -0,0 +1,14 @@
+cd dwm-6.5
+sudo make clean install
+
+cd ..
+cd dmenu-5.3
+sudo make clean install
+
+cd .. 
+cd slstatus-1.0
+sudo make clean install
+
+cd ..
+cd st-0.9.2
+sudo make clean install
